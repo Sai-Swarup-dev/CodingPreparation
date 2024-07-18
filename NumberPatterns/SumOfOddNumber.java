53865e9bd2d7b8b4be99b0880689456b35b8d798
Sai-Swarup-dev <saiswaruppatnaik14@gmail.com>
2024-07-18 11:41:38 +0530

    Sum of Odd Number

---
+++ NumberPatterns/SumOfOddNumber.java
@@ -0,0 +1,21 @@
+package NumberPatterns;
+
+import java.util.Scanner;
+
+public class SumOfOddNumber {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the N to get the series: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        System.out.print("Odd Number Series: ");
+        for(int i=1; i<n; i++){
+            if(i%2 != 0){
+                sum = sum+i;
+                System.out.print(sum+" ");
+            }
+        }
+    }
+}
