fa05c81ec7fcd4e984dbb337fd1b08bebb3fac00
Sai-Swarup-dev <saiswaruppatnaik14@gmail.com>
2024-07-18 11:36:56 +0530

    Sum of Even Number

---
+++ NumberPatterns/SumOfEvenNumbers.java
@@ -0,0 +1,21 @@
+package NumberPatterns;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the N till which you want the series: ");
+        int N = sc.nextInt();
+        int sum = 0;
+
+        System.out.print("Sum of Even Number Series: ");
+        for(int i=1; i<N; i++){
+            if(i%2 == 0){
+                sum = sum + i;
+                System.out.print(sum+" ");
+            }
+        }
+    }
+}
